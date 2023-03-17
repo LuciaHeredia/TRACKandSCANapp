01bbfe11183f8ee791e76f5cd287933b124e5d7f
Lucia Heredia <LuciaHeredia22@gmail.com>
2023-03-18 01:39:31 +0200

    basics(act+frag+nav+menu)- DONE

---
+++ app/src/main/java/com/example/tracknscan/activities/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.tracknscan.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.tracknscan.R
+import com.ismaeldivita.chipnavigation.ChipNavigationBar
+
+class MainActivity : AppCompatActivity() {
+
+    private val menu by lazy { findViewById<ChipNavigationBar>(R.id.bottom_menu_bar) }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Initialize: Navigation + Bottom Menu
+        initNavigationAndMenu()
+    }
+
+    private fun initNavigationAndMenu() {
+        // Navigation
+        val navigationHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navigationHost.navController
+
+        // Bottom Menu
+        menu.setItemSelected(R.id.map,true) // init tapped item
+        menu.setOnItemSelectedListener(object : ChipNavigationBar.OnItemSelectedListener {
+            override fun onItemSelected(id: Int) {
+                when (id) {
+                    R.id.map -> R.color.select_icons to navController.navigate(R.id.action_bluetoothFragment_to_mapFragment)
+                    R.id.bluetooth -> R.color.select_icons to navController.navigate(R.id.action_mapFragment_to_bluetoothFragment)
+                    else -> R.color.white to ""
+                }
+            }
+        })
+
+    }
+
+}
